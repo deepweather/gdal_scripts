f61cb07992911826ffc03ef03d79688f00faf2f9
predly <58434418+predly@users.noreply.github.com>
2021-01-06 11:39:41 +0100

    Update grib_cli.sh

--- scripts/grib_cli.sh
+++ scripts/grib_cli.sh
@@ -12,11 +12,14 @@ elif [ $1 -gt 588 ]; then
 	echo 1>&2 "band number must be between 1 and 588"
 else
 
-for i in $(ls -d $2); 
+echo $(ls $2)
+
+for i in $(ls $2); 
 do 
   if [[ "$i" == *"pgrb2"* ]]; then 
-  gdal_translate $i -b $1 ${i:end:-24}${i:22:2}_tmp.grib2 && gdalwarp -s_srs "+proj=longlat +ellps=WGS84" -t_srs WGS84 -te -180 -90 180 90  ${i:end:-24}${i:22:2}_tmp.grib2 ${i:end:-24}${i:22:2}_resampled.grib2 -wo SOURCE_EXTRA=1000
-  rm ${i:end:-24}${i:22:2}_tmp.grib2
+  echo $i
+  gdal_translate $2/$i -b $1 $2/${i:22:2}_tmp.grib2 && gdalwarp -s_srs "+proj=longlat +ellps=WGS84" -t_srs WGS84 -te -180 -90 180 90  $2/${i:22:2}_tmp.grib2 $3/${i:22:2}_resampled.grib2 -wo SOURCE_EXTRA=1000
+  rm $2/${i:22:2}_tmp.grib2
   fi
 done
 fi
