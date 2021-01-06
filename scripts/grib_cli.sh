91dff5af456537822ed5d1e0c001f5af48c50548
predly <58434418+predly@users.noreply.github.com>
2021-01-06 12:41:35 +0100

    Update grib_cli.sh

--- scripts/grib_cli.sh
+++ scripts/grib_cli.sh
@@ -4,22 +4,17 @@ elif [ $# -gt 3 ]; then
 	echo 1>&2 "Too many arguments. Usage: grib_cli.sh [band_number] [input_folder] [output_prefix]"
 elif [ $# -eq 3 ]; then
 
-echo $1
-
 if [ $1 -lt 1 ]; then
         echo 1>&2 "band number must be between 1 and 588"
 elif [ $1 -gt 588 ]; then
 	echo 1>&2 "band number must be between 1 and 588"
 else
 
-echo $(ls $2)
-
 for i in $(ls $2); 
 do 
-  if [[ "$i" == *"pgrb2"* ]]; then 
-  echo $i
-  gdal_translate $2/$i -b $1 $2/${i:22:2}_tmp.grib2 && gdalwarp -s_srs "+proj=longlat +ellps=WGS84" -t_srs WGS84 -te -180 -90 180 90  $2/${i:22:2}_tmp.grib2 $3/${i:22:2}_resampled.grib2 -wo SOURCE_EXTRA=1000
-  rm $2/${i:22:2}_tmp.grib2
+  if [[ "$i" == *"pgrb2"* ]] && [[ "$i" != *".f000"* ]]; then 
+  gdal_translate $2/$i -b $1 $3/${i:21:3}_tmp_${1}.grb2 && gdalwarp -s_srs "+proj=longlat +ellps=WGS84" -t_srs WGS84 -te -180 -90 180 90  $3/${i:21:3}_tmp_${1}.grb2 $3/${i:21:3}_resampled_${1}.grb2 -wo SOURCE_EXTRA=1000
+  rm $3/${i:21:3}_tmp_${1}.grb2
   fi
 done
 fi
