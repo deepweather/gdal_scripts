26febabc6b1e5def53f4a985f32b1fb7f29cecf8
neo <marvingabler@outlook.de>
2021-01-10 17:58:01 +0100

    Added comp script

---
+++ scripts/extract_tif_band_compressed.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+if [ $# -lt 1 ]; then
+	echo 1>&2 "$0: use band number"
+elif [ $# -gt 1 ]; then
+	echo 1>&2 "$0: Too many arguments."
+elif [ $# -eq 1 ]; then
+
+echo $1
+
+if [ $1 -lt 1 ]; then
+        echo 1>&2 "band number must be between 1 and 588"
+elif [ $1 -gt 588 ]; then
+	echo 1>&2 "band number must be between 1 and 588"
+else
+
+for i in $(ls); 
+do 
+  if [[ "$i" == *"pgrb2"* ]]; then 
+  gdal_translate $i -b $1 -ot Int16 -of GTiff -co COMPRESS=DEFLATE -co PREDICTOR=2 -co ZLEVEL=9 ${i:22:2}_tmp.tif && gdalwarp -s_srs "+proj=longlat +ellps=WGS84" -t_srs WGS84 -te -180 -90 180 90  ${i:22:2}_tmp.tif ${i:22:2}_tmp_resampled.tif -wo SOURCE_EXTRA=1000
+  rm ${i:22:2}_tmp.tif
+  fi
+done
+fi
+fi
